de09a5c18822fd2d17bca5372936f49ce2a994c4
lysuytry <lysuytry20@gmail.com>
2018-07-26 14:21:56 +0700

    s

--- src/models/user.js
+++ src/models/user.js
@@ -75,8 +75,7 @@ export const joinChannel = async (id, socket) => {
       { $set: { socketId: socket.id } },
       { new: true }
     );
-    //return new object after update
-    if (socket.id !== socketId) return new Error('Id is invalid.');
+    if(!socketId) return new Error('Id is invalid.');
     socket.join(channelId);
     const count = await countUserInChannel(channelId);
     io.of('/chatroom')
